a7a08e613659dc1314cc4852cad1aa9ecc8eabf5
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-11-25 17:05:09 +0700

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,19 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+x = Person("John", "Doe")
+x.printname()
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+x = Student("Mike", "Olsen", 2019)
+x.printname()
+x.welcome()
