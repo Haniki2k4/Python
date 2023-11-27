bdd86db85271f60e62f338431c3351606099c13d
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-11-27 22:54:21 +0700

    Create SETs data type.py

---
+++ SETs data type.py
@@ -0,0 +1,8 @@
+"""
+__________________________________INTPUT____________________________________________________
+Để có thể chạy mà không có lỗi thì nên xóa những phần comment """comment""" và #comment
+
+Đặt là một bộ sưu tập không có thứ tự, không thể thay đổi (nhưng bạn có thể xóa và/hoặc thêm các mục bất cứ khi nào bạn thích.), 
+      và không được chỉ định. Không có thành viên trùng lặp.
+"""
+
