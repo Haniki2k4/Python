e56bf676c99d777f191f42ae66567452ad68dea3
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-11-28 21:04:54 +0700

    Create Strings Methods.py

---
+++ Strings Methods.py
@@ -0,0 +1,100 @@
+'''
+____________String Methods_______(vietnamese below)________
+'''
+
+capitalize() #Converts the first character to upper case 
+casefold() #Converts string into lower case 
+center() #Returns a centered string
+count() #Returns the number of times a specified value occurs in a string 
+encode() #Returns an encoded version of the string 
+endswith() #Returns true if the string ends with the specified value 
+expandtabs() #Sets the tab size of the string 
+find() #Searches the string for a specified value and returns the position of where it was found 
+format() #Formats specified values in a string 
+format_map() #Formats specified values in a string 
+index() #Searches the string for a specified value and returns the position of where it was found 
+isalnum() #Returns True if all characters in the string are alphanumeric 
+isalpha() #Returns True if all characters in the string are in the alphabet 
+isascii() #Returns True if all characters in the string are ascii characters 
+isdecimal() #Returns True if all characters in the string are decimals 
+isdigit() #Returns True if all characters in the string are digits 
+isidentifier() #Returns True if the string is an identifier 
+islower() #Returns True if all characters in the string are lower case 
+isnumeric() #Returns True if all characters in the string are numeric 
+isprintable() #Returns True if all characters in the string are printable 
+isspace() #Returns True if all characters in the string are whitespaces 
+istitle() #Returns True if the string follows the rules of a title 
+isupper() #Returns True if all characters in the string are upper case 
+join() #Joins the elements of an iterable to the end of the string 
+ljust() #Returns a left justified version of the string 
+lower() #Converts a string into lower case 
+lstrip() #Returns a left trim version of the string 
+maketrans() #Returns a translation table to be used in translations 
+partition() #Returns a tuple where the string is parted into three parts 
+replace() #Returns a string where a specified value is replaced with a specified value 
+rfind() #Searches the string for a specified value and returns the last position of where it was found
+rindex() #Searches the string for a specified value and returns the last position of where it was found 
+rjust() #Returns a right justified version of the string 
+rpartition() #Returns a tuple where the string is parted into three parts 
+rsplit() #Splits the string at the specified separator, and returns a list 
+rstrip() #Returns a right trim version of the string 
+split() #Splits the string at the specified separator, and returns a list 
+splitlines() #Splits the string at line breaks and returns a list 
+startswith() #Returns true if the string starts with the specified value 
+strip() #Returns a trimmed version of the string 
+swapcase() #Swaps cases, lower case becomes upper case and vice versa 
+title() #Converts the first character of each word to upper case 
+translate() #Returns a translated string 
+upper() #Converts a string into upper case 
+zfill() #Fills the string with a specified number of 0 values at the beginning
+
+'''
+__________Vietsub____________
+'''
+
+capitalize()  #Chuyển ký tự đầu tiên thành chữ hoa
+casefold()  #Chuyển chuỗi thành chữ thường
+center() #Trả về một chuỗi ở giữa
+count() #Trả về số lần một giá trị được chỉ định xuất hiện trong một chuỗi
+encode() #Trả về phiên bản đã mã hóa của chuỗi
+endswith() #Trả về true nếu chuỗi kết thúc bằng giá trị đã chỉ định
+expandtabs() #Đặt kích thước tab của chuỗi
+find() #Tìm kiếm chuỗi cho một giá trị được chỉ định và trả về vị trí nơi nó được tìm thấy
+format()  #Định dạng các giá trị được chỉ định trong một chuỗi
+format_map() #Định dạng các giá trị được chỉ định trong một chuỗi
+index()  #Tìm kiếm chuỗi cho một giá trị được chỉ định và trả về vị trí nơi nó được tìm thấy
+isalnum() #Trả về đúng nếu tất cả ký tự trong chuỗi là chữ và số
+isalpha() #Trả về Đúng nếu tất cả các ký tự trong chuỗi đều nằm trong bảng chữ cái
+isascii() #Trả về Đúng nếu tất cả các ký tự trong chuỗi là ký tự ascii
+isdecimal()  #Trả về đúng nếu tất cả ký tự trong chuỗi là số thập phân
+isdigit()  #Trả về đúng nếu tất cả ký tự trong chuỗi là chữ số
+isidentifier() #Trả về True nếu chuỗi là định danh
+islower() #Trả về Đúng nếu tất cả các ký tự trong chuỗi là chữ thường
+isnumeric() #Trả về đúng nếu tất cả ký tự trong chuỗi là số
+isprintable() #Trả về Đúng nếu tất cả các ký tự trong chuỗi đều có thể in được
+isspace()   #Trả về Đúng nếu tất cả ký tự trong chuỗi là khoảng trắng
+istitle()  #Trả về Đúng nếu chuỗi tuân theo quy tắc của tiêu đề
+isupper()  #Trả về Đúng nếu tất cả các ký tự trong chuỗi đều là chữ hoa
+join()   #Nối các phần tử của một iterable vào cuối chuỗi
+ljust() #Trả về phiên bản canh lề trái của chuỗi
+lower()  #Chuyển chuỗi thành chữ thường
+lstrip()  #Trả về phiên bản cắt bên trái của chuỗi
+maketrans() #Trả về bảng dịch được sử dụng trong bản dịch
+partition()  #Trả về một bộ trong đó chuỗi được chia thành ba phần
+replace() #Trả về một chuỗi trong đó giá trị đã chỉ định được thay thế bằng giá trị đã chỉ định
+rfind()  #Tìm kiếm chuỗi cho một giá trị được chỉ định và trả về vị trí cuối cùng nơi nó được tìm thấy
+rindex()  #Tìm kiếm chuỗi cho một giá trị được chỉ định và trả về vị trí cuối cùng nơi nó được tìm thấy
+rjust() #Trả về phiên bản canh lề phải của chuỗi
+rpartition()  #Trả về một bộ trong đó chuỗi được chia thành ba phần
+rsplit() #Tách chuỗi tại dấu phân cách đã chỉ định và trả về một danh sách
+rstrip() #Trả về phiên bản cắt bên phải của chuỗi
+split() #Tách chuỗi tại dấu phân cách đã chỉ định và trả về một danh sách
+splitlines()  #Tách chuỗi tại dấu ngắt dòng và trả về một danh sách
+startswith()  #Trả về true nếu chuỗi bắt đầu bằng giá trị đã chỉ định
+strip() #Trả về phiên bản rút gọn của chuỗi
+swapcase() #Hoán đổi trường hợp, chữ thường trở thành chữ hoa và ngược lại
+title()  #Chuyển ký tự đầu tiên của mỗi từ thành chữ hoa
+translate() #Trả về một chuỗi đã dịch
+upper() #Chuyển một chuỗi thành chữ hoa
+zfill() #Điền chuỗi với số giá trị 0 được chỉ định ở đầu
+
