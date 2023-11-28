b3a0cf33b38560ccc7de62cb34ef3874f8cadd3e
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-11-28 21:12:59 +0700

    Update LIST data types.py

--- LIST data types.py
+++ LIST data types.py
@@ -4,6 +4,18 @@ __________________________________INTPUT________________________________________
 
 List là một bộ sưu tập được sắp xếp trình tự và có thể thay đổi. Cho phép các thành viên trùng lặp.
 """
+append()	#Adds an element at the end of the list
+clear()	#Removes all the elements from the list
+copy()	#Returns a copy of the list
+count()	#Returns the number of elements with the specified value
+extend()	#Add the elements of a list (or any iterable), to the end of the current list
+index() #Returns the index of the first element with the specified value
+insert()	#Adds an element at the specified position
+pop()	#Removes the element at the specified position
+remove()	#Removes the item with the specified value
+reverse()	#Reverses the order of the list
+sort()	#Sorts the list
+
 #Các kiểu dữ liệu chuỗi, int và boolean:
 list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
